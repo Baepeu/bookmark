b604286971023b70e9dafa8b719d43b628b7fa53
Beapeu <beap.jg@gmail.com>
2020-05-12 20:49:21 +0900

    deploy

---
+++ bookmark/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # http://127.0.0.1/bookmark/
+    #
+    path("", BookmarkListView.as_view(), name='list'),
+    path("add/", BookmarkCreateView.as_view(), name='add'),
+    path("detail/<int:pk>/", BookmarkDetailView.as_view(), name='detail'),
+    path("update/<int:pk>/", BookmarkUpdateView.as_view(), name='update'),
+    path("delete/<int:pk>/", BookmarkDeleteView.as_view(), name='delete'),
+]
---
+++ bookmark/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+# CRUD : Create, Read, Update, Delete
+# List
+
+# 클래스형 뷰, 함수형 뷰
+# 웹 페이지에 접속한다. -> 페이지를 본다.
+# URL을 입력 -> 웹 서버가 뷰를 찾아서 동작시킨다. -> 응답
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+from django.urls import reverse_lazy
+from .models import Bookmark
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name','url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name','url']
+    template_name_suffix = '_update'
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
